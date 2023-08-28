9089b1592637aec4df0a71c2de78797b657fc366
ian-bravo <bravo.ian@gmail.com>
2023-08-28 16:15:54 -0700

    final

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
-import 'bootstrap';
-import './css/styles.css';
 import Triangle from "./triangle.js";
+
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './css/styles.css';
 
 function handleTriangleForm() {
   event.preventDefault();
